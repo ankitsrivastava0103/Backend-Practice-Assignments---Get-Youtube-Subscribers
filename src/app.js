37025d4ac6e6a741d8c4896ed3d22963cfe3e50c
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2020-12-10 19:47:06 +0530

    Chnages

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
 
+  
 const express = require('express');
-const app = express()
+const app = express();
+const Subscriber = require("./models/subscribers");
 
 
-// Your code goes here
+app.get("/subscribers", (req, res) => {
+    Subscriber.find().then(data => res.send(data));
+})  
 
+app.get("/subscribers/names", (req, res) => {
+    Subscriber.find().then((data) => {
+        let arr = [];
+        data.map( ele => {
+            let name = ele.name;
+            let subscribedChannel = ele.subscribedChannel;
+            arr.push({name, subscribedChannel});
+        });
+        res.send(arr);
+    });
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/subscribers/:id", (req, res) => {
+    Subscriber.find({_id: req.params.id})
+        .then(data => res.send(data[0]))
+        .catch(err => res.status(400).send({message: err.message}));
+})
 
 module.exports = app;
